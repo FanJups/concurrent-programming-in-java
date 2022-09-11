f3a156a181130825172109277722314079647ad7
Fanon Jupkwo <jupsfan@gmail.com>
2022-09-11 18:10:01 +0100

    Success Mini Project 3

--- src/main/java/edu/coursera/concurrent/SieveActor.java
+++ src/main/java/edu/coursera/concurrent/SieveActor.java
@@ -1,6 +1,9 @@
 package edu.coursera.concurrent;
 
 import edu.rice.pcdp.Actor;
+import static edu.rice.pcdp.PCDP.finish;
+import static edu.rice.pcdp.PCDP.async;
+
 
 /**
  * An actor-based implementation of the Sieve of Eratosthenes.
@@ -19,7 +22,37 @@ public final class SieveActor extends Sieve {
      */
     @Override
     public int countPrimes(final int limit) {
-        throw new UnsupportedOperationException();
+	
+	final SieveActorActor sieveActorActor = new SieveActorActor(2);
+	
+	finish(()->{
+	    
+		
+		  for (int i = 3; i <= limit; i += 2) {
+		            
+		            sieveActorActor.send(i);  
+		            
+		            }
+	    
+	});
+	
+	//sieveActorActor.send(0); 
+	
+	int numPrimes =0;
+	SieveActorActor loopActor = sieveActorActor;
+	while(loopActor !=null) {
+	    
+	    numPrimes += loopActor.numLocalPrimes;
+	    loopActor = loopActor.nextActor();
+	    
+	}
+	
+        
+        
+        return numPrimes;
+
+       
+	
     }
 
     /**
@@ -27,6 +60,29 @@ public final class SieveActor extends Sieve {
      * parallel.
      */
     public static final class SieveActorActor extends Actor {
+	
+	private static final int MAX_LOCAL_PRIMES = 1000;
+	private final int localPrimes[];
+	private int numLocalPrimes;
+	private SieveActorActor nextActor;
+	
+	SieveActorActor(final int localPrime){
+	    
+	    this.localPrimes = new int[MAX_LOCAL_PRIMES];
+	    this.localPrimes[0] = localPrime;
+	    this.numLocalPrimes=1;
+	    this.nextActor =null;
+	    
+	}
+	
+	public SieveActorActor nextActor() {
+	    return nextActor;
+	}
+	
+	public int numLocalPrimes() {
+	    return numLocalPrimes;
+	}
+
         /**
          * Process a single message sent to this actor.
          *
@@ -36,7 +92,65 @@ public final class SieveActor extends Sieve {
          */
         @Override
         public void process(final Object msg) {
-            throw new UnsupportedOperationException();
+            
+            final int candidate = (Integer)msg;
+            
+            if(candidate <=0) {
+        	
+        	if(nextActor != null) {
+        	    
+        	    nextActor.send(msg);
+        	    
+        	}
+        	//exit();
+        	
+            }else {
+        	
+        	final boolean locallyPrime = isLocallyPrime(candidate);
+        	if(locallyPrime) {
+        	    
+        	    if(numLocalPrimes < MAX_LOCAL_PRIMES) {
+        		
+        		localPrimes[numLocalPrimes] = candidate;
+        		numLocalPrimes +=1;
+        		
+        	    }else if(nextActor == null){
+        		
+        		nextActor = new SieveActorActor(candidate);
+        		//nextActor.start();
+        	    }else {
+        		
+        		nextActor.send(msg);
+        	    }
+        	    
+        	}
+        	
+        	
+            }
         }
+        
+        private boolean isLocallyPrime(final int candidate) {
+            
+            final boolean[] isPrime = {true};
+            checkPrimeKernel(candidate,isPrime,0,numLocalPrimes);
+            return isPrime[0];
+        }
+        
+        private void checkPrimeKernel(final int candidate, final boolean[] isPrime,final int startIndex,final int endIndex ) {
+            
+            for(int i=startIndex; i<endIndex;i++) {
+        	
+        	if(candidate % localPrimes[i] ==0) {
+        	    
+        	    isPrime[0]=false;
+        	    //break;
+        	    
+        	}
+        	
+            }
+            
+          
+        }
+       
     }
 }
